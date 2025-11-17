6050e4cd4ccc3aa475d99751c1b32ee66db0893c
Maria Julia Felix <mariajuliaf160@gmail.com>
2025-11-17 09:51:11 -0300

    Delete burg_go directory

--- burg_go/js/domain.js
+++
@@ -1,1 +0,0 @@
-export const URL_BASE_API = "https://sponsor.mexpenses.com.br";
--- burg_go/js/headerHam.js
+++
@@ -1,7 +0,0 @@
-const hamburgerIcon = document.querySelector('.headerHam');
-const navMenu = document.querySelector('.nav-menu');
-
-hamburgerIcon.addEventListener('click', () => {
-  navMenu.classList.toggle('active');   
-  hamburgerIcon.classList.toggle('open'); 
-});
